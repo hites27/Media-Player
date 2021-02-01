7b3959eadfe9218f69745fcb8b0a64a4947be24f
Hites Chitalia <hites27@gmail.com>
2021-02-01 10:11:20 +0530

    Moved files into  source

--- src/script.js
+++ src/script.js
@@ -4,21 +4,21 @@ const manifestUri = [
   {
     name: "Star Trek",
     desc: "Star trek in spanish",
-    img: "Star.jpg",
+    img: "assets/Star.jpg",
     videoUrl:
       "https://storage.googleapis.com/shaka-demo-assets/angel-one/dash.mpd",
   },
   {
     name: "Big Buck",
     desc: "A documentary on Big buck",
-    img: "Big.png",
+    img: "assets/Big.png",
     videoUrl:
       "https://storage.googleapis.com/shaka-demo-assets/bbb-dark-truths-hls/hls.m3u8",
   },
   {
     name: "Angel One",
     desc: "Star trek demo version (Espanol)",
-    img: "Angle one.jpg",
+    img: "assets/Angle one.jpg",
     videoUrl:
       "https://storage.googleapis.com/shaka-demo-assets/angel-one/dash.mpd",
   },
